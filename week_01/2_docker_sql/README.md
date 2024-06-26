8fa1f2ebc4d6f4a0f976969b9cf9eda50b61e186
Wes MacDonald <wrmacdonald2016@gmail.com>
2024-06-26 13:59:38 -0700

    Continue Week 1
    - Connecting pgAdmin and Postgres
    - Dockerizing the Ingestion Script
    - Running Postgres & pgAdmin with Docker-Compose

--- week_01/2_docker_sql/README.md
+++ week_01/2_docker_sql/README.md
@@ -47,3 +47,97 @@ pgcli -h localhost -p 5432 -u root -d ny_taxi
 ```
 
 
+## Connecting pgAdmin and Postgres
+
+Install pgAdmin in Docker:
+
+```bash
+docker run -it \
+  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+  -e PGADMIN_DEFAULT_PASSWORD="root" \
+  -p 8080:80 \
+  dpage/pgadmin4
+```
+
+Create Docker Network so pgAdmin & Postgres can communicate:
+
+```bash
+docker network create pg-network
+```
+
+Update running postgres in Docker, to use network:
+
+```bash
+docker run -it \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
+  -p 5432:5432 \
+  --network=pg-network \
+  --name pg-database \
+  postgres:13
+```
+
+Update running pgAdmin in Docker, to use network:
+
+```bash
+docker run -it \
+  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+  -e PGADMIN_DEFAULT_PASSWORD="root" \
+  -p 8080:80 \
+  --network=pg-network \
+  --name pgadmin \
+  dpage/pgadmin4
+```
+
+
+## Dockerizing the Ingestion Script
+
+Run python ingestion script
+
+```bash
+URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+python ingest_data.py \
+    --user=root \
+    --password=root \
+    --host=localhost \
+    --port=5432 \
+    --db=ny_taxi \
+    --url=${URL} \
+    --table_name=yellow_taxi_trips
+```
+
+Build python ingestion script container: taxi_ingest
+
+```bash
+docker build -t taxi_ingest:v001 .
+```
+
+Run taxi_ingest container:
+
+```bash
+URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+docker run -it \
+  --network=pg-network \
+  taxi_ingest:v001 \
+    --user=root \
+    --password=root \
+    --host=pg-database \
+    --port=5432 \
+    --db=ny_taxi \
+    --table_name=yellow_taxi_trips \
+    --url=${URL}
+```
+
+## Running Postgres and pgAdmin with Docker-Compose
+
+```bash
+docker compose up
+```
+
+```bash
+docker compose down
+```
---
+++ week_01/2_docker_sql/ingest_data.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    url = params.url
+    table_name = params.table_name
+
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+
+    # download the csv
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    while True: 
+        t_start = time()
+        
+        df = next(df_iter)
+        
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+
+        t_end = time()
+
+        print(f'inserted another chunk..., took {t_end - t_start:.3f}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres.')
+
+    # user, password, host, port, database name, table name, url_of_csv
+    parser.add_argument('--user', help='username for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='table name in postgres to write results to')
+    parser.add_argument('--url', help='url of csv file')
+
+    args = parser.parse_args()
+
+    main(args)
